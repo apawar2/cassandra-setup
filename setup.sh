cda4c85e78477151a3931c87053eeb1948f16de5
Ankita Pawar <apawar2@ncsu.edu>
2015-09-13 14:33:16 -0400

    initial file commits

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+mkdir -p ~/opt/packages
+cd ~/opt/packages
+
+#Download JAVA 7 for Cassandra setup
+wget --no-cookies \
+--no-check-certificate \
+--header "Cookie: oraclelicense=accept-securebackup-cookie" \
+"http://download.oracle.com/otn-pub/java/jdk/7u79-b15/jdk-7u79-linux-x64.tar.gz"
+#cp -r /vagrant/jdk-7u79-linux-x64.gz .
+tar -xzvf jdk-7u79-linux-x64.tar.gz
+sudo mkdir /usr/lib/jvm
+sudo mv jdk1.7.0_79 /usr/lib/jvm
+sudo update-alternatives --install "/usr/bin/java" "java"     "/usr/lib/jvm/jdk1.7.0_79/bin/java" 1
+sudo update-alternatives --install "/usr/bin/javac" "javac"     "/usr/lib/jvm/jdk1.7.0_79/bin/javac" 1
+sudo update-alternatives --install "/usr/bin/javaws" "javaws"     "/usr/lib/jvm/jdk1.7.0_79/bin/javaws" 1
+java -version
+javac -version
+javaws -version
+wget http://mirrors.koehn.com/apache/cassandra/2.2.1/apache-cassandra-2.2.1-bin.tar.gz
+gzip -dc apache-cassandra-2.2.1-bin.tar.gz | tar xf -
+ln -s ~/opt/packages/apache-cassandra-2.2.1 ~/opt/cassandra
+mkdir -p ~/opt/cassandra/data/data
+mkdir -p ~/opt/cassandra/data/commitlog
+mkdir -p ~/opt/cassandra/data/saved_caches
+mkdir -p ~/opt/cassandra/logs
+. ./env.sh
+#export PATH=$PATH:~/opt/cassandra/bin
+~/opt/cassandra/bin/cassandra
+
